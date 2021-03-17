6708beb660cf07b21fe5ec07b9a4a7df6af0920b
darkodekan <darko.dekan@protonmail.com>
2021-03-17 21:01:24 +0100

    komit

---
+++ lekcija19.py
@@ -0,0 +1,32 @@
+# github.com
+
+# https://github.com/acoustid/chromaprint
+
+# https://github.com/zachary822/audio_compare/blob/master/audio_compare.py
+
+# git 
+
+# git clone https://github.com/zachary822/audio_compare/blob/master/audio_compare.py
+
+
+# registracija github
+
+
+# py2exe
+
+# requests
+# logging
+# pandas
+# matplotlib
+
+
+
+print("hello!")
+
+
+
+# pravljenje lokalnog repozitorijuma
+# git init
+
+# dodavanje
+# git add .
